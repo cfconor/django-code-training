d5aab858c004d2bf537ef1479ca63078888a87b3
cfconor <cfconor@gmail.com>
2021-05-24 11:15:29 +0000

    added heroku app to allowed hosts

--- django_todo/django_todo/settings.py
+++ django_todo/django_todo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-8827fnwp$m2n2bxw8w1u_%+4#xr3_qw*me@^*%v8h+)$o@%v^i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://cfconor-django-app.herokuapp.com/']
 
 
 # Application definition
